d9d4dd640e0991e41f68dd7766d5a6a684e9c6af
Jester <jeremytruong0204@gmail.com>
2024-09-25 16:27:20 +0700

    initial commit

---
+++ nest-auth/src/app.controller.ts
@@ -0,0 +1,92 @@
+import { BadRequestException, Body, Controller, Get, Post, Req, Res, UnauthorizedException } from '@nestjs/common';
+import { AppService } from './app.service';
+import { JwtService } from '@nestjs/jwt';
+import * as bcrypt from 'bcrypt';
+import { Response, Request } from 'express';
+import { PassThrough } from 'stream';
+
+@Controller('api')
+export class AppController {
+  constructor(
+    private readonly appService: AppService,
+    private readonly jwtService: JwtService 
+  ) {
+
+  }
+
+  @Post('register') 
+  async register(
+    @Body('name') name: string,
+    @Body('email') email: string,
+    @Body('password') password: string,
+  ) {
+    const hashedPassword = await bcrypt.hash(password, 12);
+
+    const user = await this.appService.create({
+      name,
+      email,
+      password: hashedPassword,
+    });
+
+    delete user.password;
+
+    return user;
+  }
+
+  @Post('login')
+  async login(
+    @Body('email') email: string,
+    @Body('password') password: string,
+    @Res({passthrough: true}) response: Response
+  ) {
+    const user = await this.appService.findOne({where: {email}});
+
+    if (!user) {
+      throw new BadRequestException('invalid credentials');
+    }
+
+    if (!await bcrypt.compare(password, user.password)) {
+      throw new BadRequestException('invalid credentials');
+    }
+
+    const jwt = await this.jwtService.signAsync({id: user.id});
+
+    response.cookie('jwt', jwt, {httpOnly: true});
+
+    return {
+      message: 'success'
+    };
+  }
+
+  @Get('user')
+  async user(@Req() request: Request) {
+      try {
+          const cookie = request.cookies['jwt'];
+
+          const data = await this.jwtService.verifyAsync(cookie);
+
+          if (!data) {
+              throw new UnauthorizedException();
+          }
+
+          const user = await this.appService.findOne({ where : {id: data['id']} } );
+
+          const {password, ...result} = user;
+
+          return result;
+      } catch (e) {
+          throw new UnauthorizedException();
+      }
+  }
+
+  @Post('logout')
+  async logout(@Res({passthrough: true}) response: Response) {
+      response.clearCookie('jwt');
+
+      return {
+          message: 'success'
+      };
+  }
+}
+
+
---
+++ next-auth/layouts/Layout.tsx
@@ -0,0 +1,75 @@
+import React from "react";
+import Head from "next/head";
+import Link from "next/link";
+import {useRouter} from 'next/router';
+
+const Layout = (props) => {
+    const router = useRouter();
+    
+    const logout = async () => {
+        await fetch('http://localhost:8000/api/logout', {
+            method: 'POST',
+            headers: {'Content-Type': 'application/json'},
+            credentials: 'include'
+        });
+
+
+        await router.push('/login');
+    }
+
+    let menu;
+
+    if(!props.auth) {
+        menu = (
+            <ul className="navbar-nav me-auto mb-2 mb-md-0">
+              <li className="nav-item">
+                    <Link href="/login" legacyBehavior>
+                        <a className="nav-link active" aria-current="page" >Login</a>
+                    </Link>
+                </li>
+                <li className="nav-item">
+                    <Link href="/register" legacyBehavior>
+                        <a className="nav-link active" aria-current="page" >Register</a>
+                    </Link>
+                </li>
+            </ul>              
+        )
+    } else {
+        menu = (
+            <ul className="navbar-nav me-auto mb-2 mb-md-0">
+                    <li className="nav-item">
+                        <a href="#" className="nav-link active" aria-current="page" onClick={logout} >Logout</a>
+                    </li>
+            </ul>
+        )
+    }
+
+    return (
+        <>
+            <Head>
+                <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" 
+                rel="stylesheet" 
+                integrity="sha384-QWTKZyjpPEjISv5WaRU9OFeRpok6YctnYmDr5pNlyT2bRjXh0JMhjY6hW+ALEwIH" 
+                crossOrigin="anonymous"/>
+            </Head>
+
+            <nav className="navbar navbar-expand-md navbar-dark bg-dark mb-4">
+                <div className="container-fluid">
+                    <Link href="/" legacyBehavior>
+                        <a className="navbar-brand">Home</a>
+                    </Link>
+
+                    <div>
+                        {menu}
+                    </div>
+                </div>
+            </nav>
+
+            <main className="form-signin w-100 m-auto">
+                {props.children}
+            </main>
+    </>
+    );
+};
+
+export default Layout;
